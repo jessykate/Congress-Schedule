1b3663b2fbc3237c973742908898eb001b7e0f6f
Jessy <jessy@moltencore.(none)>
2010-03-28 15:36:26 -0400

    turned house floor parser into a class. still needs work to pull out bill urls and parse lists properly (since it turns out they're not actually html lists most of the time)

--- schedules.py
+++ schedules.py
@@ -6,66 +6,151 @@ except:
     import simplejson as json
 
 from BeautifulSoup import BeautifulSoup
-import urllib2
+import urllib2, sys
 
 
-def house_floor_current():
-    fp = urllib2.urlopen("http://majorityleader.gov/links_and_resources/whip_resources/currentdailyleader.cfm")
-    soup = BeautifulSoup(fp.read())
+class HouseFloorSchedule(object):
+    def __init__(self, date='today'):
+        ''' initializes a house floor schedule object with various
+        information needed to parse the html, and then retrieves the
+        html.'''
 
-    # a note about navigating the parse tree with beautiful soup: dont
-    # want to be too dependent on the existing page structure by
-    # specifying every element of every step. on the other hand, want
-    # to be specific enough that newly introduced elements don't break
-    # our parsing scheme.
+        if date != 'today':
+            print 'not implemented.'
+            sys.exit()
 
-    js = {
-        'date': None,
-        'start_meet': None,
-        'start_vote': None,
-        'end_vote': None,
-        'agenda': None        
-        }
+        # prefix notes appear before te actual schedule items
+        self.announcements_prefix = "members are advised"
 
-    date_header = soup.table.findNext('tbody').findNext('tr').findNext('td').findNext('td').findNext('strong').text
-    # the date format seems to be fine for javascript parsers, so leave it as it. 
-    # eg. SATURDAY, MARCH 20, 2010
-    js['date'] = date_header.split('FLOOR SCHEDULE FOR ')[1]    
+        # schedule headings are not structurally unique in the html
+        # but seem to adhere to a consistent set of text
+        # strings. note: 'suspension' sometimes appears in the
+        # singular so leave it as such here to catch both cases.
+        self.schedule_heading_prefixes = ["one minutes", 
+                                          "suspension", 
+                                          "postponed suspension", 
+                                          "dispose of", 
+                                          "complete consideration", 
+                                          "begin consideration", 
+                                          "conference report", 
+                                          "continue consideration", 
+                                          "motion to",
+                                          "possible consideration",
+                                          "h.r.", 
+                                          "h.con.res.", 
+                                          "h.res", 
+                                          "h.j.res",     
+                                          ]
+    
+        # these appear to be at the bottom of each day's schedule. 
+        self.schedule_footnotes = ["conference reports may be brought up at any time", 
+                                   "motions to go to conference should they become available", 
+                                   "possible motions to instruct conferees"]
 
-    # get start and end times for this session 
-    js['start_meet'] = soup.table.findNext('tbody').findNext('table').findNext('tbody').findNext('tr').findNext('strong').findNext('span').text
-    js['start_vote'] = soup.table.findNext('tbody').findNext('table').findNext('tbody').findNext('tr').findNext('strong').findNext('strong').findNext('span').text
-    js['end_vote'] = soup.table.findNext('tbody').findNext('table').findNext('tbody').findNext('tr').findNext('strong').findNext('strong').findNext('strong').findNext('span').text
+        self.get_html(date)
 
-    # get the agenda as one blob of text. we do this because
-    # apparently we don't believe in headers anymore, so there's no
-    # good way to consistently differentiate between categories of
-    # agenda items and the agenda items themselves.
-    agenda = soup.table.findNext('tbody').findNext('tr').findNext('td').findNext('div', unselectable="off")
-    # take out all the retarded markup
-    for elem in agenda.findAll(True):
-        elem.attrs = []
-    # fix the escaped chars, then get rid of extra whitespace and the
-    # now-useless <span> elements. 
-    for elem in agenda.findAll(True):
-        if elem.name == 'span':
-            # get the children and replace the span element with its own subtree
+    def parse(self):
+        ''' builds a dictionary with the schedule information from html'''
+        self.schedule = {
+            'date': None,
+            'start_meet': None,
+            'start_vote': None,
+            'end_vote': None,
+            'agenda': None        
+            }
+        self.get_date()
+        self.get_start_meet()
+        self.get_start_vote()
+        self.get_end_vote()
+        self.get_agenda()
+        # pull out links to bills on thomas
+        # (find links for opencongress?)
+        # 
+        return self.schedule
+
+    def as_json(self):
+        return json.dumps(self.parse())
+
+    def get_html(self, date):
+        ''' get the html markup corresponding to the schedule on the date specified'''
+        fp = urllib2.urlopen(self.get_schedule_url(date))
+        self.soup = BeautifulSoup(fp.read())
+
+    def get_schedule_url(self, date):
+        # call this url with the start parameter equal to the number of
+        # records back to go. ie, the most current agenda will show up
+        # when called with start=1. so with start=n, the top record
+        # displayed on the page will be that from n-1 days ago.
+        # 
+        # http://majorityleader.gov/links_and_resources/whip_resources/dailyleader.cfm?start=2
+
+        url = "http://majorityleader.gov/links_and_resources/whip_resources/currentdailyleader.cfm"
+        #url = "http://majorityleader.gov/links_and_resources/whip_resources/dailyleader.cfm?pressReleaseID=4022"
+        return url
         
-       
+    def get_date(self):
+        date_header = (self.soup.table.findNext('tbody').findNext('tr').
+                       findNext('td').findNext('td').findNext('strong').text)
+        # the date format seems to be fine for javascript parsers, so
+        # leave it as it. eg. SATURDAY, MARCH 20, 2010
+        self.schedule['date'] = date_header.split('FLOOR SCHEDULE FOR ')[1]    
+            
+    def get_start_meet(self):
+        self.schedule['start_meet'] = (self.soup.table.findNext('tbody').findNext('table').
+                                       findNext('tbody').findNext('tr').
+                                       findNext('strong').findNext('span').text)
 
-    return json.dumps(js)
+    def get_start_vote(self):
+        self.schedule['start_vote'] = (self.soup.table.findNext('tbody').findNext('table').
+                                       findNext('tbody').findNext('tr').findNext('strong').
+                                       findNext('strong').findNext('span').text)
 
-def house_floor(date='today'):
-    if date == 'today':
-        return house_floor_current()
-    # call this url with the start parameter equal to the number of
-    # records back to go. ie, the most current agenda will show up
-    # when called with start=1. so with start=n, the top record
-    # displayed on the page will be that from n-1 days ago.
-    # 
-    # http://majorityleader.gov/links_and_resources/whip_resources/dailyleader.cfm?start=2
-        
-    # grab the top url and follow it. 
+    def get_end_vote(self):
+        self.schedule['end_vote'] = (self.soup.table.findNext('tbody').findNext('tbody').
+                                     findNext('tr').findNext('td').findNext('strong').
+                                     findNext('strong').findNext('strong').text)
+
+    def check_for_announcements(self, text):
+        if self.announcements_prefix in text:
+            self.schedule['agenda']['announcements'].append(text)
+            return True
+        return False
+
+    def check_for_footnotes(self, text):
+        for footnote in self.schedule_footnotes:
+            if footnote in text:
+                self.schedule['agenda']['footnotes'].append(text)                    
+                return True
+        return False
+
+    def add_to_agenda_item(self, item, text):
+
+        # if it has a parent tag that is a list item then it's not a
+        # section heading. (should add search for parent style that's
+        # an "mso-list" such as on nov.6, 2009. 
+        if not item.findParent('li'):
+            # check to see if this matches any known headings. if so,
+            # start a new (empty) schedule item.
+            for heading in self.schedule_heading_prefixes:
+                if heading in text:
+                    self.schedule['agenda']['items'].append([])
+
+        # whatever the current schedule item, append the text to
+        # it. if it was determined above to be a heading, then this
+        # will be the first line for this item. else, it will just be
+        # appended to the current item.
+        self.schedule['agenda']['items'].insert(-1, item.text)
+
+    def get_agenda(self):
+        self.schedule['agenda'] = {'announcements': [], 'items': [], 'footnotes': []}
+        agenda = (self.soup.table.findNext('tbody').findNext('tr').
+                  findNext('td').findNext('div', unselectable="off"))
+        for item in agenda.findAll('span'):
+            # normalize case and strip whitespace
+            text = item.text.lower().strip()
+            if not self.check_for_announcements(text):
+                if not self.check_for_footnotes(text):
+                    self.add_to_agenda_item(item, text)
 
 
 def senate_floor_current():
@@ -78,5 +163,27 @@ def senate_committee_current():
     pass
 
 if __name__ == '__main__':
-    print 'house floor current schedule\n'
-    print house_floor_current()
+    print 'House Floor Current Schedule\n'
+    house_sched = HouseFloorSchedule("today")
+    sched = house_sched.as_json()
+    for k,v in json.loads(sched).iteritems():
+        print "========== %s ============" % k
+        if k == 'agenda':
+            if v['announcements']:
+                print "========== Announcements ============"
+                for item in v['announcements']:
+                    print item
+
+            if v['items']:
+                print "========== Agenda Items ============"
+                for item in v['items']:
+                    print item
+
+            if v['footnotes']:
+                print "========== Footnote Items ============"
+                for item in v['footnotes']:
+                    print item
+
+        else:
+            print v
+            print ""
