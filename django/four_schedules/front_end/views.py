e3d417173bcb7e60cc614d8d1b8418e5c486cf87
Joshua Ruihley <ruihley@gmail.com>
2010-04-13 20:29:30 +0000

    fixed view conflicts

--- django/four_schedules/front_end/views.py
+++ django/four_schedules/front_end/views.py
@@ -3,13 +3,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.core.urlresolvers import reverse
 from django.template import RequestContext
-
-<<<<<<< HEAD:django/four_schedules/front_end/views.py
-from four_schedules.parsers.schedules import HouseFloorSchedule, SenateFloorSchedule
-=======
-from four_schedules.scrapers.schedules import HouseFloorSchedule
->>>>>>> b61ab5b6b4b7332b6f862c5d34be92e496bf58ff:django/four_schedules/front_end/views.py
+from four_schedules.scrapers.schedules import HouseFloorSchedule, SenateFloorSchedule
 
 def index(request):
     senate_floor = SenateFloorSchedule().as_json()
-    return render_to_response('index.html', {'senate_floor': senate_floor})
+    return render_to_response('front_end/index.html', {'senate_floor': senate_floor})
