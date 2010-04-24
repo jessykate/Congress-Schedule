e49f505d893200833bd2eb5f6d947260aa52b722
Joshua Ruihley <ruihley@gmail.com>
2010-04-24 19:21:01 +0000

    added committee schedules to view and template

--- django/four_schedules/front_end/views.py
+++ django/four_schedules/front_end/views.py
@@ -1,3 +1,4 @@
+import pymongo
 from django.conf import settings
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
@@ -7,14 +8,25 @@ from four_schedules.scrapers.schedules import (HouseFloorSchedule,
                                                SenateFloorSchedule,
                                                SenateCommitteeSchedule,
                                                HouseCommitteeSchedule)
+from pymongo import Connection
+
+
+def committee_schedule(chamber):
+    connection = Connection()
+    db = connection.schedules
+    collection = db.committee_hearing
+    events = []
+    for event in collection.find({'chamber':chamber}):
+        events.append(event)
+    return events                                  
 
 def index(request):
     house_floor_url = "http://majorityleader.gov/links_and_resources/whip_resources/currentdailyleader.cfm"
     senate_floor_url = "http://www.senate.gov/pagelayout/legislative/d_three_sections_with_teasers/calendars.htm"
     senate_floor = SenateFloorSchedule().parse()
     house_floor = HouseFloorSchedule().parse()
-    senate_cmte = SenateCommitteeSchedule().parse()
-    house_cmte = SenateCommitteeSchedule().parse()
+    senate_cmte = committee_schedule('Senate')
+    house_cmte = committee_schedule('House')
     return render_to_response('front_end/index.html', 
                               {'senate_floor': senate_floor, 
                                'house_floor': house_floor,
