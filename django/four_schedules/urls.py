8e00708ffcc8e5125ce5459b234aa03c0d50c75a
jessykate <jessy.cowansharp@gmail.com>
2010-04-24 18:26:10 -0400

    starting to format things

--- django/four_schedules/urls.py
+++ django/four_schedules/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
+
+    url(r'^%s/(?P<path>.*)$' % settings.MEDIA_URL.strip('/'), 
+        'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True }),        
     url(r'^$', 'front_end.views.index', name='index'),
 )
